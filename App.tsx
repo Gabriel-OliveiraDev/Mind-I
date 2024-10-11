1a23c94e424232995946be3650d99a7618f70a3d
Gabriel Oliveira <gabrieloliveira663@icloud.com>
2024-10-11 14:45:11 -0300

    Atualização do arquivo App

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import Routes from './src/routes'
+import { AppProvider } from './src/context'
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
   )
 }
